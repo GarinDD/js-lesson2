356defcaabad83de09b92ec264965134436c333c
Daniel Garin <streetbossog@gmail.com>
2023-09-01 19:13:48 +0300

    Cycle4

--- script2.js
+++ script2.js
@@ -1,13 +1,13 @@
 
- x = +prompt("Input number from 1 to 6");
- switch(x){
- case 2:
- case 4:
- case 6: alert("even");
- break;
- case 1:
- case 3:
- case 5: alert("odd");
- default: alert("Out of range");
- }
+x = +prompt("Input number from 1 to 6");
+switch (x) {
+    case 2:
+    case 4:
+    case 6: alert("even");
+        break;
+    case 1:
+    case 3:
+    case 5: alert("odd");
+    default: alert("Out of range");
+}
 
