ab152b4996b2c61a9c027a00202fccad8a5c3ab2
Daniel Garin <streetbossog@gmail.com>
2023-09-01 20:28:15 +0300

    breakcontinue

---
+++ script7.js
@@ -0,0 +1,35 @@
+// let sum = +prompt("Сколько хочешь откладывать?")
+// let month = +prompt("Сколько месяцев?")
+
+// let resultSum = 0
+
+
+// fot(i= 0, resultSum = 0; i< month; i++) {
+//     resultSum = resultSum +sum
+
+//     if(i % 2 ==0) {
+//         resultSum -= 100;
+//     }
+
+// }
+// alert(resultSum)
+
+// for (i =0, s="*"; i<5;i++, s=s+"*") {
+//     console.log(s)
+// // } 
+// arr=[1,2,3,4,5]
+// for(i in arr) console.log(i)
+
+for (i = 0; i < 10; i++) {
+    rnd = Math.round(Math.random() * 6) + 1;
+    if (rnd == 4) {
+        alert("Выпало 4");
+        break;
+    };
+    if (rnd == 5) {
+        alert("Выпало 5");
+        continue;
+    };
+    console.log(rnd);
+}
+console.log("Конец")
