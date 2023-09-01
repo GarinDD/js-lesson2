d6ed211cef64babd3f1d3b86cb0fc6464ed36929
Daniel Garin <streetbossog@gmail.com>
2023-09-01 18:57:44 +0300

    Cycle2

---
+++ script.js
@@ -0,0 +1,5 @@
+let number = 10
+let result = number % 2 == 0 ? "четное" : "не четное";
+let resString = number + " является " + result;
+console.log(number + " является " + result)
+console.log(resString)
---
+++ script1.js
@@ -0,0 +1,13 @@
+switch(protocol){
+    case "HTTP":
+    description = "Hypertext transfer protocol";
+    break;
+    case "HTTPS":
+    description = "Secure hypertext transfer protocol";
+    break;
+    case "FTP":
+    description = "File transfer protocol";
+    break;
+    default :
+    description = "Unsupported protocol";
+   }
